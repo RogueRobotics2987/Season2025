4cd30ac306e73e57991c2c5371685fc7494ab0e4
= <=>
2025-01-16 15:41:15 -0600

    wrote some comments how to switch state in the state machine

--- src/main/cpp/subsystems/CoralSubsystem.cpp
+++ src/main/cpp/subsystems/CoralSubsystem.cpp
@@ -8,26 +8,36 @@ CoralSubsystem::CoralSubsystem() = default;
 
 // This method will be called once per scheduler run
 void CoralSubsystem::Periodic() {
+    // Update Sensors
+
     switch (_state) {
 
         case EMPTY:
-            //code
+            // code
+            // claw ready to grab coral
+            // if funnel BB == true (change state to CORAL_IN_FUNNEL)
             break;
 
         case CORAL_IN_FUNNEL:
-            //code
+            // code
+            // if trough BB == true (change state to CORAL_IN_TROUGH)
             break;
 
         case CORAL_IN_TROUGH:
-            //code
+            // code
+            // if claw BB == true (change state to ALLOW_CORAL_MOVE)
             break;
 
         case ALLOW_CORAL_MOVE:
-            //code
+            // code
+            // change lights
+            // coral_place == true ?? (change state to CORAL_PLACE)
             break;
 
         case CORAL_PLACE:
-            //code
+            // code
+            // arm swing down to place coral
+            // if claw BB == false && armPose == lowered (change state to EMPTY)
             break;
 
         default:
