83dce0999a04c90c066c93adae4e44ce8b8139c9
= <=>
2025-02-01 10:32:12 -0600

    Added code to get values from cameras

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -2,11 +2,25 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+#include <iostream>
 #include "Robot.h"
-
 #include <frc2/command/CommandScheduler.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+using namespace std;
+
+nt::DoubleArraySubscriber positionSub;
+nt::DoubleArraySubscriber orientationSub;
 
 Robot::Robot() {}
+
+void Robot::RobotInit() {
+
+    // Add to your RobotInit, Command, or Subsystem
+  auto table = nt::NetworkTableInstance::GetDefault().GetTable("MAPLE");
+  positionSub = table->GetDoubleArrayTopic("position").Subscribe({});
+  orientationSub = table->GetDoubleArrayTopic("orientation").Subscribe({});
+}
 
 void Robot::RobotPeriodic() {
   frc2::CommandScheduler::GetInstance().Run();
@@ -36,7 +50,24 @@ void Robot::TeleopInit() {
   }
 }
 
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopPeriodic() {
+  
+    // Get the latest value for position and orientation. If the value hasn't been updated since the last time
+   // we read the table, use the same value.
+  std::vector<double> position = positionSub.Get();
+  std::vector<double> orientation = orientationSub.Get();
+
+  std::cout << "position XYZ: ";
+  for (double pos : position) {
+    std::cout << pos << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "orientation RPY: ";
+  for (double angle : orientation) {
+    std::cout << angle << " ";  }
+  std::cout << std::endl;
+}
 
 void Robot::TeleopExit() {}
 
