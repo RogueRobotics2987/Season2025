b04436cdb979c2aebc651f4d1e1cb3d706c7f417
PGRandCNW <jonahfrank90@gmail.com>
2025-02-08 11:25:28 -0600

    Removed code that wasn't being used

--- src/main/include/TeleopCurve.h
+++ src/main/include/TeleopCurve.h
@@ -1,3 +1,9 @@
+// Program will give robot has more control at lower speeds
+// We will do this with the equation y=x^3
+// 1. Make a class called TeleopCurve
+// 2. In this class you will have one function with a return type of double, one parameter of type double, and the function should be called apply.
+// 3. The apply function should calculate the equation y=x^3 and return the value y. x is the parameter of the function.
+
 class TeleopCurve{
     public:
     TeleopCurve(){};
@@ -7,3 +13,5 @@ class TeleopCurve{
         return output;
     }
 };
+
+// TeleopCurve::apply(joystick.GetLeftX());
