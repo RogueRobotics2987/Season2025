3ad34576c10aad686f6ef1afaeae7c658fbb57bf
Calise Edeen <caliseedeen@gmail.com>
2025-02-16 14:20:05 -0600

    commented out auto chooser switched pointers back to the way they were

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -143,7 +143,7 @@ void RobotContainer::ConfigureBindings() // more needs to be added somewhere in 
 
 frc2::CommandPtr RobotContainer::GetAutonomousCommand()
 {
-    return m_chooser.GetSelected(); //*m_chooser compiles when this is not being returned //pointers make me sad ): //cant covert from a raw pointer to a smart pointer
+   // return m_chooser.GetSelected(); //*m_chooser compiles when this is not being returned //pointers make me sad ): //cant covert from a raw pointer to a smart pointer
 
     //auto path = PathPlannerPath::fromPathFile("TestMoveOutPath");
 
