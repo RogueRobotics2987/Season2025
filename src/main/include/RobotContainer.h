52e2f1be0b93fad06aecb91b7b931c9eb0b310c8
BrandonS2987 <brandonsandmeier@icloud.com>
2025-02-22 14:57:44 -0600

    Mapped buttons and added drive buttons

--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -8,6 +8,7 @@
 #include <frc2/command/button/CommandXboxController.h>
 #include "Constants.h"
 #include "subsystems/CoralSubsystem.h"
+#include "subsystems/ClimberSubsystem.h"
 #include "subsystems/CommandSwerveDrivetrain.h"
 #include "Telemetry.h"
 #include <frc2/command/RunCommand.h>
@@ -58,6 +59,7 @@ public:
 
   // The robot's subsystems are defined here...
   CoralSubsystem m_coralSubsystem;
+  ClimberSubsystem m_climberSubsystem;
 
 
   void ConfigureBindings();
