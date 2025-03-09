063df2baf0f29526a2ab78aa188bf62071c26f98
= <=>
2025-03-09 18:55:26 -0500

    changed climber button code

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -101,14 +101,14 @@ void RobotContainer::ConfigureBindings()
                                    m_coralSubsystem.SetIntakeMotors(0);
                                })
                                    .ToPtr());
-    
-        AuxStick.X().ToggleOnTrue(frc2::InstantCommand([this]() -> void {
-            m_coralSubsystem.SetClimber(0.1);
-        }).ToPtr());
-      
-        AuxStick.X().ToggleOnFalse(frc2::InstantCommand([this]() -> void {
-            m_coralSubsystem.SetClimber(0);
-        }).ToPtr());
+
+    AuxStick.X().ToggleOnTrue(frc2::InstantCommand([this]() -> void
+                                                   { m_coralSubsystem.SetClimber(-0.1); })
+                                  .ToPtr());
+
+    AuxStick.X().ToggleOnFalse(frc2::InstantCommand([this]() -> void
+                                                    { m_coralSubsystem.SetClimber(0); })
+                                   .ToPtr());
 
     // Run SysId routines when holding back/start and X/Y.
     // Note that each routine should be run exactly once in a single log.
